5ffd3e3eb4cd0b90cd5473f5e1f9fb08822e4204
Cédric MONCHICOURT <cmonchicourt@auchan.net>
2017-07-05 19:34:02 +0200

    Ajout de la classe société

---
+++ src/Societe.java
@@ -0,0 +1,24 @@
+/**
+ * Created by CMONCHICOURT on 05/07/2017.
+ */
+public class Societe {
+
+private String nom;
+private String lieu;
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public String getLieu() {
+        return lieu;
+    }
+
+    public void setLieu(String lieu) {
+        this.lieu = lieu;
+    }
+}
